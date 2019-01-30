942f91b3a7535e2eef4c24b74ca1b7c51647bbb9
Tyler Kraft <thekraftkrew@gmail.com>
2019-01-30 15:24:52 -0600

    added options

--- nextcloudblock.py
+++ nextcloudblock.py
@@ -4,15 +4,23 @@ import pymysql
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("dbuser", type=str, help="database user used to connect to nextcloud database")
-parser.add_argument("dbpassword", type=str, help="password for database user used to connect to nextcloud database")
-parser.add_argument("db", type=str, help="nextcloud database name")
-parser.add_argument("dbhost", type=str, help="database server ip")
-parser.add_argument("outputfile", type=str, help="path and file name of output file")
+parser.add_argument("-u", "--dbuser", required=True, type=str,
+                    help="database user used to connect to nextcloud database")
+parser.add_argument("-p", "--dbpassword", required=True, type=str,
+                    help="password for database user used to connect to nextcloud database")
+parser.add_argument("-d", "--db", required=True, type=str,
+                    help="nextcloud database name")
+parser.add_argument("-s", "--dbserver", required=True,
+                    type=str, help="database server ip")
+parser.add_argument("-o", "--outputfile", required=False,
+                    type=str, help="path and file name of output file")
+parser.add_argument("-b", "--unban", required=False,
+                    type=str, help="ip address to unban")
 
 args = parser.parse_args()
 
-dbconnection = pymysql.connect(host=args.dbhost, user=args.dbuser, password=args.dbpassword, db=args.db, cursorclass=pymysql.cursors.DictCursor)
+dbconnection = pymysql.connect(host=args.dbserver, user=args.dbuser,
+                               password=args.dbpassword, db=args.db, cursorclass=pymysql.cursors.DictCursor)
 
 
 path, filename = os.path.split(args.outputfile)
