c4b280800ebabc56f4c0783c8dafffb15755f161
Tyler Kraft <thekraftkrew@gmail.com>
2019-01-30 16:23:10 -0600

    unban code

--- nextcloudblock.py
+++ nextcloudblock.py
@@ -6,42 +6,57 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-u", "--dbuser", required=True, type=str,
                     help="database user used to connect to nextcloud database")
+
 parser.add_argument("-p", "--dbpassword", required=True, type=str,
-                    help="password for database user used to connect to nextcloud database")
+                    help="password for database user")
+
 parser.add_argument("-d", "--db", required=True, type=str,
                     help="nextcloud database name")
+
 parser.add_argument("-s", "--dbserver", required=True,
                     type=str, help="database server ip")
+
 parser.add_argument("-o", "--outputfile", required=False,
                     type=str, help="path and file name of output file")
+
 parser.add_argument("-b", "--unban", required=False,
                     type=str, help="ip address to unban")
 
 args = parser.parse_args()
 
-dbconnection = pymysql.connect(host=args.dbserver, user=args.dbuser,
-                               password=args.dbpassword, db=args.db, cursorclass=pymysql.cursors.DictCursor)
+if args.outputfile:
 
+    dbconnection = pymysql.connect(host=args.dbserver, user=args.dbuser,
+                                   password=args.dbpassword, db=args.db,
+                                   cursorclass=pymysql.cursors.DictCursor)
 
-path, filename = os.path.split(args.outputfile)
+    path, filename = os.path.split(args.outputfile)
 
-os.chdir(path)
+    os.chdir(path)
 
-bannedips = open(filename, 'w')
+    bannedips = open(filename, 'w')
 
-cursor = dbconnection.cursor()
+    cursor = dbconnection.cursor()
 
-cursor.execute("SELECT * FROM oc_bruteforce_attempts;")
+    cursor.execute("SELECT * FROM oc_bruteforce_attempts;")
 
-results = cursor.fetchall()
+    results = cursor.fetchall()
 
-iplist = set()
+    iplist = set()
 
-for i in results:
-    if i['ip'] not in iplist:
+    for i in results:
+        if i['ip'] not in iplist:
 
-        bannedips.write(i["ip"] + "\n")
+            bannedips.write(i["ip"] + "\n")
 
-        iplist.add(i["ip"])
+            iplist.add(i["ip"])
 
-bannedips.close()
+    bannedips.close()
+
+else:
+
+    cursor = dbconnection.cursor()
+
+    unbansql = "DELETE FROM oc_bruteforce_attempts WHERE ip = " + args.unban
+
+    cursor.execute(unbansql)
