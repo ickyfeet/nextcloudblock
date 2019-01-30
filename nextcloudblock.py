d80f112d8f2376a75e920327b1984dc65b744365
Tyler Kraft <thekraftkrew@gmail.com>
2019-01-30 11:58:49 -0600

    Clean up

--- nextcloudblock.py
+++ nextcloudblock.py
@@ -4,11 +4,11 @@ import pymysql
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("dbuser",type=str,help="database user used to connect to nextcloud database")
-parser.add_argument("dbpassword",type=str,help="password for database user used to connect to nextcloud database")
-parser.add_argument("db",type=str,help="nextcloud database name")
-parser.add_argument("dbhost",type=str,help="database server ip")
-parser.add_argument("outputfile",type=str,help="path and file name of output file")
+parser.add_argument("dbuser", type=str, help="database user used to connect to nextcloud database")
+parser.add_argument("dbpassword", type=str, help="password for database user used to connect to nextcloud database")
+parser.add_argument("db", type=str, help="nextcloud database name")
+parser.add_argument("dbhost", type=str, help="database server ip")
+parser.add_argument("outputfile", type=str, help="path and file name of output file")
 
 args = parser.parse_args()
 
