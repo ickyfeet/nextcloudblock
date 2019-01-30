4df23a1d5056542ddadfcd91e2caf396c5379ce6
Tyler Kraft <thekraftkrew@gmail.com>
2019-01-30 17:01:38 -0600

    Documentation

--- nextcloudblock.py
+++ nextcloudblock.py
@@ -2,7 +2,17 @@ import os
 import argparse
 import pymysql
 
+"""Initialize arparse"""
 parser = argparse.ArgumentParser()
+
+"""Argument Parser:
+    --dbuser:  Database user, required
+    --dbpassword:  Password for the db user, required
+    --db:  Database to work on
+    --dbserver:  Database server to connect to, required
+    --outputfile:  File to ouput list to, optional
+    --unban:  IP address to unban
+"""
 
 parser.add_argument("-u", "--dbuser", required=True, type=str,
                     help="database user used to connect to nextcloud database")
@@ -22,25 +32,33 @@ parser.add_argument("-o", "--outputfile", required=False,
 parser.add_argument("-b", "--unban", required=False,
                     type=str, help="ip address to unban")
 
+"""Assign arguments to args"""
+
 args = parser.parse_args()
+
+"""Declare PyMySql database connection"""
+
+dbconnection = pymysql.connect(host=args.dbserver, user=args.dbuser,
+                               password=args.dbpassword, db=args.db,
+                               cursorclass=pymysql.cursors.DictCursor)
+
+"""If there's an output file connect to database and pull IP address"""
 
 if args.outputfile:
 
-    dbconnection = pymysql.connect(host=args.dbserver, user=args.dbuser,
-                                   password=args.dbpassword, db=args.db,
-                                   cursorclass=pymysql.cursors.DictCursor)
+    path, filename = os.path.split(args.outputfile) #Split file name from pat
 
-    path, filename = os.path.split(args.outputfile)
+    os.chdir(path) #Change Directory to path
 
-    os.chdir(path)
+    bannedips = open(filename, 'w') #Open the file
 
-    bannedips = open(filename, 'w')
+    cursor = dbconnection.cursor() #Connect to database server
 
-    cursor = dbconnection.cursor()
+    cursor.execute("SELECT * FROM oc_bruteforce_attempts;") #Execute the sql query
 
-    cursor.execute("SELECT * FROM oc_bruteforce_attempts;")
+    results = cursor.fetchall() #Fetch all results
 
-    results = cursor.fetchall()
+    """Initialize the list of IP addresses and remove duplicates and write to file"""
 
     iplist = set()
 
@@ -52,11 +70,17 @@ if args.outputfile:
             iplist.add(i["ip"])
 
     bannedips.close()
-
+    
 else:
+
+    """Unban the IP address provided from the command line arguments"""
 
     cursor = dbconnection.cursor()
 
-    unbansql = "DELETE FROM oc_bruteforce_attempts WHERE ip = " + args.unban
+    unbansql = "DELETE FROM oc_bruteforce_attempts WHERE ip = '" + args.unban + "';"
 
     cursor.execute(unbansql)
+
+    dbconnection.commit()
+
+    print('Unbanned ip address ' + args.unban)
